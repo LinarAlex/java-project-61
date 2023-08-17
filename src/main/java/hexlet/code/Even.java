316819d7a5438041ed73360b77358616a731133d
LinarAlex <linch.nn@gmail.com>
2023-08-17 23:46:14 +0300

    Build step 5

--- src/main/java/hexlet/code/Even.java
+++ src/main/java/hexlet/code/Even.java
@@ -11,33 +11,31 @@ public class Even {
         System.out.println("Hello, " + userName + "!");
         System.out.println("Answer 'yes' if the number is even, otherwise answer 'no'.");
         int count = 0;
-            while (count < 3) {
-                int getRandomNumber = (int) (Math.random() * 100);
-                System.out.println("Question: " + getRandomNumber);
-                System.out.print("Your answer: ");
-                String playerAnswer = scanner.nextLine();
-                if ((getRandomNumber % 2 == 0) & playerAnswer.equals("yes")) {
+        while (count < 3) {
+            int getRandomNumber = (int) (Math.random() * 100);
+            System.out.println("Question: " + getRandomNumber);
+            System.out.print("Your answer: ");
+            String playerAnswer = scanner.nextLine();
+            if ((getRandomNumber % 2 == 0) & playerAnswer.equals("yes")) {
+                System.out.println("Correct");
+                count++;
+            } else
+                if ((getRandomNumber % 2 != 0) & playerAnswer.equals("no")) {
                     System.out.println("Correct");
                     count++;
-                } else
-                    if ((getRandomNumber % 2 !=0) & playerAnswer.equals("no")) {
-                        System.out.println("Correct");
-                        count++;
-                    }
-                if ((getRandomNumber % 2 == 0) & !(playerAnswer.equals("yes"))) {
-                    System.out.println(playerAnswer + " is wrong answer ;(. Correct answer was 'yes'.");
-                    System.out.println("Let's try again, " + userName);
-                    break;
-                } else if ((getRandomNumber % 2 !=0) & !(playerAnswer.equals("no"))) {
-                    System.out.println(playerAnswer + " is wrong answer ;(. Correct answer was 'no'.");
-                    System.out.println("Let's try again, " + userName);
-                    break;
                 }
+            if ((getRandomNumber % 2 == 0) & !(playerAnswer.equals("yes"))) {
+                System.out.println(playerAnswer + " is wrong answer ;(. Correct answer was 'yes'.");
+                System.out.println("Let's try again, " + userName);
+                break;
+            } else if ((getRandomNumber % 2 != 0) & !(playerAnswer.equals("no"))) {
+                System.out.println(playerAnswer + " is wrong answer ;(. Correct answer was 'no'.");
+                System.out.println("Let's try again, " + userName);
+                break;
             }
-       if (count == 3) {
-           System.out.println("Congratulations! " + userName);
-       }
+        }
+        if (count == 3) {
+            System.out.println("Congratulations! " + userName);
+        }
     }
-
-
 }
