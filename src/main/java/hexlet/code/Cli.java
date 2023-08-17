0aa78ffaa457025ec060b117a9b8df1d163d337b
LinarAlex <linch.nn@gmail.com>
2023-08-17 23:28:55 +0300

    Step 5

--- src/main/java/hexlet/code/Cli.java
+++ src/main/java/hexlet/code/Cli.java
@@ -3,11 +3,11 @@ import java.util.Scanner;
 
 public class Cli {
     public static void askName() {
+        System.out.println("Welcome to the Brain Games!");
         Scanner scanner = new Scanner(System.in);
         System.out.print("May I have your name? ");
         String userName = scanner.nextLine();
         System.out.println("Hello, " + userName + "!");
-        Greet.gameGreetStart(userName);
     }
 
 
