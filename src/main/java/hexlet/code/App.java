374f9f3d2725aa83def7ed7598dbb567705efb7a
LinarAlex <linch.nn@gmail.com>
2023-08-17 14:15:28 +0300

    Add checkstyle and Cli class

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -1,8 +1,19 @@
 package hexlet.code;
-
+import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
-        System.out.println("Welcome to the Brain Games!");
-        Cli.askName();
+        int gameNumber1 = 1;
+        int gameNumber0 = 0;
+        System.out.println("Please enter the game number and press Enter.");
+        System.out.println(gameNumber1 + " - Greet");
+        System.out.println(gameNumber0 + " - Exit");
+        Scanner scanner = new Scanner(System.in);
+        int gameChoice = scanner.nextInt();
+        if (gameChoice == gameNumber1) {
+            System.out.println("Welcome to the Brain Games!");
+            Cli.askName();
+            Greet.gameGreetStart("Bill");
+        }
+        scanner.close();
     }
 }
--- src/main/java/hexlet/code/Cli.java
+++ src/main/java/hexlet/code/Cli.java
@@ -5,8 +5,10 @@ public class Cli {
     public static void askName() {
         Scanner scanner = new Scanner(System.in);
         System.out.print("May I have your name? ");
-        String userName = scanner.next();
+        String userName = scanner.nextLine();
         System.out.println("Hello, " + userName + "!");
-        scanner.close();
+        Greet.gameGreetStart(userName);
     }
+
+
 }
