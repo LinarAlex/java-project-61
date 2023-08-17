55003930aadead26634188b16c730199e31175f5
LinarAlex <linch.nn@gmail.com>
2023-08-17 23:54:01 +0300

    Final build of step 5

--- src/main/java/hexlet/code/Even.java
+++ src/main/java/hexlet/code/Even.java
@@ -17,25 +17,25 @@ public class Even {
             System.out.print("Your answer: ");
             String playerAnswer = scanner.nextLine();
             if ((getRandomNumber % 2 == 0) & playerAnswer.equals("yes")) {
-                System.out.println("Correct");
+                System.out.println("Correct!");
                 count++;
             } else
                 if ((getRandomNumber % 2 != 0) & playerAnswer.equals("no")) {
-                    System.out.println("Correct");
+                    System.out.println("Correct!");
                     count++;
                 }
             if ((getRandomNumber % 2 == 0) & !(playerAnswer.equals("yes"))) {
                 System.out.println(playerAnswer + " is wrong answer ;(. Correct answer was 'yes'.");
-                System.out.println("Let's try again, " + userName);
+                System.out.println("Let's try again, " + userName + "!");
                 break;
             } else if ((getRandomNumber % 2 != 0) & !(playerAnswer.equals("no"))) {
                 System.out.println(playerAnswer + " is wrong answer ;(. Correct answer was 'no'.");
-                System.out.println("Let's try again, " + userName);
+                System.out.println("Let's try again, " + userName + "!");
                 break;
             }
         }
         if (count == 3) {
-            System.out.println("Congratulations! " + userName);
+            System.out.println("Congratulations! " + userName + "!");
         }
     }
 }
